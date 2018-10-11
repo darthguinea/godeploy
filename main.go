abb64982817a923878031553f13a7bb9f3c38eb4
ade <darthguinea@protonmail.com>
2018-10-11 17:43:11 +1100

    Inital

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"./src/cfn"
+	"./src/log"
+)
+
+type stringFlag struct {
+	set   bool
+	value string
+}
+
+func (sf *stringFlag) Set(x string) error {
+	sf.value = x
+	sf.set = true
+	return nil
+}
+
+func (sf *stringFlag) String() string {
+	return sf.value
+}
+
+func main() {
+	var (
+		flagName         stringFlag
+		flagURI          stringFlag
+		flagRegion       string
+		flagUpdate       bool
+		flagNoChangeSet  bool
+		flagListStacks   bool
+		flagCapabilities string
+		flagVerbose      bool
+	)
+	flag.Var(&flagName, "n", "<stack_name> Stack name to use")
+	flag.BoolVar(&flagUpdate, "u", false, "Allow stack to be updated if stack exists")
+	flag.BoolVar(&flagNoChangeSet, "x", false, "A change set is created by default, use this flag if you want to update without creating a changeset")
+	flag.Var(&flagURI, "f", "<location> Cloudformation location, i.e. file://./cfn.yaml or s3://location")
+	flag.BoolVar(&flagListStacks, "l", false, "List stacks")
+	flag.StringVar(&flagRegion, "r", "us-west-1", "Region")
+	flag.StringVar(&flagCapabilities, "a", "", "<CAPABILITIES> list of capabilities i.e. CAPABILITY_IAM,CAPABILITY_NAMED_IAM")
+	flag.BoolVar(&flagVerbose, "v", true, "verbose messaging")
+	flag.Parse()
+
+	if flagVerbose {
+		log.SetLevel(log.DEBUG)
+		log.Debug("verbose messaging enabled")
+	}
+
+	if flagListStacks {
+		log.Debug("Listing stacks in region %v", flagRegion)
+		cfn.DescribeStacks(flagRegion)
+		os.Exit(0)
+	}
+
+	if flagName.set && flagURI.set {
+		params := flag.Args()
+		log.Debug("Parameters passed in: %v", params)
+		if exists, stackDetails := cfn.StackExists(flagRegion, flagName.value); exists {
+			// if Update stack is true
+			log.Info("Stack %v exists in state %v", *stackDetails.StackName, *stackDetails.StackStatus)
+			if flagUpdate {
+				if flagNoChangeSet {
+					// if No Change set is set (just update without changeset)
+					log.Warn("No Change set flag set, updating stack...")
+				} else {
+					// Create change set
+					log.Info("Creating change set")
+					cfn.CreateChangeSet(flagRegion, flagName.value, flagURI.value, params)
+				}
+			} else {
+				log.Info("Update flag not set, exiting.")
+				os.Exit(-1)
+			}
+		} else {
+			// cfn.CreateStack(flagRegion, flagName.value, flagURI.value, params)
+		}
+	}
+}
---
+++ src/cfn/stack.go
@@ -0,0 +1,33 @@
+package cfn
+
+import (
+	"os"
+
+	"../log"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+)
+
+func createStackFromFile(name string, uri string, params []*cloudformation.Parameter) cloudformation.CreateStackInput {
+	file, err := os.Open(uri)
+	if err != nil {
+		log.Error("Could not open file %v", uri)
+	}
+
+	data := make([]byte, 1048576)
+	x, _ := file.Read(data)
+	templateBody := string(data[:x])
+
+	return cloudformation.CreateStackInput{
+		StackName:    &name,
+		TemplateBody: &templateBody,
+		Parameters:   params,
+	}
+}
+
+func createStackFromURI(name string, uri string, params []*cloudformation.Parameter) cloudformation.CreateStackInput {
+	return cloudformation.CreateStackInput{
+		StackName:   &name,
+		TemplateURL: &uri,
+		Parameters:  params,
+	}
+}
